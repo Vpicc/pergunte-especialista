bdbe04aa0f812cba4e338468c070dad6724a7742
Victor Piccoli <victorpicc@gmail.com>
2017-05-15 12:15:27 -0300

    Adicionado suporte a funcoes crianca nas paginas

--- wp-content/themes/pergunteespecialista/functions.php
+++ wp-content/themes/pergunteespecialista/functions.php
@@ -1,18 +1,39 @@
 <?php
 
+// styles da pagina
 function pergunteEspecialista_resources(){
 
   wp_enqueue_style('style', get_stylesheet_uri());
 
 }
 
-
+//adiciona style
 add_action('wp_enqueue_scripts', 'pergunteEspecialista_resources');
 
 // Navigation Menus
 register_nav_menus(array(
-  'primary' => __( 'Menu primário'),
+  'primary' => __('Menu primário'),
   'footer' => __('Menu de rodapé'),
 ));
 
+// Pegar o ancestral da pagina
+function get_top_ancestor_id() {
+
+  global $post;
+
+  if($post->post_parent){
+    $ancestors = array_reverse(get_post_ancestors($post->ID));
+    return $ancestors[0];
+  }
+  return $post->ID;
+}
+
+// Informa se a pagina tem uma pagina crianca
+function has_children(){
+
+  global $post;
+
+  $pages = get_pages('child_of=' . $post->ID);
+  return count($pages);
+}
 ?>
--- wp-content/themes/pergunteespecialista/page.php
+++ wp-content/themes/pergunteespecialista/page.php
@@ -1,17 +1,41 @@
 <?php
 get_header();
 
-
   if(have_posts()){
-    while(have_posts()){the_post();
+    while(have_posts()){ the_post(); ?>
 
-?>
-  <article class="post page">
-    <h2><?php the_title(); ?><h2>
-    <h3><?php the_content();?><h3>
-  </article>
+    <article class="post page">
 
-<?php }} else {
+      <?php
+
+      if(has_children() OR $post->post_parent > 0){ ?>
+
+        <nav class="site-nav children-links clearfix">
+          <span class="parent-link"><a href="<?php echo get_the_permalink(get_top_ancestor_id());?>">
+            <?php echo get_the_title(get_top_ancestor_id()); ?></a></span>
+
+          <ul>
+
+              <?php
+              $args = array (
+                'child_of' => get_top_ancestor_id(),
+                'title_li' => '',
+              ); ?>
+
+              <?php wp_list_pages($args);?>
+
+          </ul>
+
+        </nav>
+
+      <?php } ?>
+
+      <h2><?php the_title(); ?><h2>
+      <h3><?php the_content();?><h3>
+    </article>
+
+<?php }}
+  else{
     echo 'Não foi encontrado nenhum post</p>';
   }
 
