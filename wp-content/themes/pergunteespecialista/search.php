1881089a3d0cc650aa086e6772018c592575b457
Victor Piccoli <victorpicc@gmail.com>
2017-05-17 12:05:05 -0300

    Adicionado fomulario de pesquisa

---
+++ wp-content/themes/pergunteespecialista/search.php
@@ -0,0 +1,57 @@
+<?php
+get_header();
+
+
+  if(have_posts()){ ?>
+    <h2>Resultados da Pesquisa: <?php the_search_query(); ?></h2>
+    <?php
+    while(have_posts()){the_post();
+
+?>
+  <article class="post <?php if(has_post_thumbnail()){ ?>has-thumbnail <?php } ?>">
+
+    <!-- post-thumbnail -->
+    <div class="post-thumbnail">
+      <a href="<?php the_permalink() ?>"><?php the_post_thumbnail('small-thumbnail'); ?></a>
+    </div><!-- /post-thumbnail -->
+
+    <h2 class="post-title"><a href="<?php the_permalink();?>"><?php the_title(); ?></a></h2>
+      <p class="post-info"><?php
+        the_time('j');
+        echo " de ";
+        the_time('F, Y');
+        echo " às ";
+        the_time('h:m');
+        ?> | Por <a href="<?php echo get_author_posts_url( get_the_author_meta( 'ID' )); ?>"><?php the_author();?></a> | Postado em
+
+        <?php $categories = get_the_category();
+        $separator = ", ";
+        $output = '';
+
+        if($categories){
+
+          foreach($categories as $category){
+
+            $output .= '<a href="' . get_category_link($category->term_id). '">'  . $category->cat_name . '</a>' . $separator;
+          }
+
+          echo trim($output, $separator);
+        }
+
+         ?>
+
+      </p>
+
+      <p><?php echo get_the_excerpt();?>
+        <a href="<?php the_permalink(); ?>">Ler mais&raquo;</a>
+      </p>
+
+  </article>
+
+<?php }} else {
+    echo 'Não foi encontrado nenhum post</p>';
+  }
+
+get_footer();
+
+ ?>
