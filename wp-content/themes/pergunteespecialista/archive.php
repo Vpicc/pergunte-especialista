3bcd3ed2bb03d6c509ad06a73859c661bcdd4d0e
Victor Piccoli <victorpicc@gmail.com>
2017-05-17 12:40:46 -0300

    Adicionado content para evitar repeticao de codigo

--- wp-content/themes/pergunteespecialista/archive.php
+++ wp-content/themes/pergunteespecialista/archive.php
@@ -30,38 +30,8 @@ get_header();
 
     <?php
     while (have_posts()){the_post();
-
+      get_template_part( 'content');
 ?>
-  <article class="post">
-    <h2 class="post-title"><a href="<?php the_permalink();?>"><?php the_title(); ?></a></h2>
-      <p class="post-info"><?php
-        the_time('j');
-        echo " de ";
-        the_time('F, Y');
-        echo " às ";
-        the_time('h:m');
-        ?> | Por <a href="<?php echo get_author_posts_url( get_the_author_meta( 'ID' )); ?>"><?php the_author();?></a> | Postado em
-
-        <?php $categories = get_the_category();
-        $separator = ", ";
-        $output = '';
-
-        if($categories){
-
-          foreach($categories as $category){
-
-            $output .= '<a href="' . get_category_link($category->term_id). '">'  . $category->cat_name . '</a>' . $separator;
-          }
-
-          echo trim($output, $separator);
-        }
-
-         ?>
-
-      </p>
-
-    <h3><?php the_excerpt();?></h3>
-  </article>
 
 <?php }} else {
     echo 'Não foi encontrado nenhum post</p>';
