f7be5ea3bba2b7bc405d3bc787878c95061a7bb6
Victor Piccoli <victorpicc@gmail.com>
2017-05-17 22:42:24 -0300

    adicionado suporte a widgets, sidebar e footer

--- wp-content/themes/pergunteespecialista/footer.php
+++ wp-content/themes/pergunteespecialista/footer.php
@@ -1,4 +1,31 @@
   <footer class="site-footer">
+    <div class="footer-widgets">
+
+      <?php if (is_active_sidebar('footer1')){ ?>
+        <div class="footer-widgets-area">
+        <?php dynamic_sidebar('footer1'); ?>
+        </div>
+      <?php } ?>
+
+      <?php if (is_active_sidebar('footer2')){ ?>
+        <div class="footer-widgets-area">
+        <?php dynamic_sidebar('footer2'); ?>
+        </div>
+      <?php } ?>
+
+      <?php if (is_active_sidebar('footer3')){ ?>
+        <div class="footer-widgets-area">
+        <?php dynamic_sidebar('footer3'); ?>
+        </div>
+      <?php } ?>
+
+      <?php if (is_active_sidebar('footer4')){ ?>
+        <div class="footer-widgets-area">
+        <?php dynamic_sidebar('footer4'); ?>
+        </div>
+      <?php } ?>
+
+    </div>
       <p><?php bloginfo('name');?> - &copy; <?php echo date('Y');?></p>
 
       <?php
--- wp-content/themes/pergunteespecialista/index.php
+++ wp-content/themes/pergunteespecialista/index.php
@@ -1,16 +1,32 @@
 <?php
-get_header();
+get_header(); ?>
+<div class="site-content clearfix">
 
+  <?php if(is_active_sidebar('sidebar1')){ ?>
+    <div class="main-column">
+      <?php
+      if(have_posts()){
+        while(have_posts()){the_post();
+          get_template_part('content', get_post_format());
+        ?>
 
+      <?php }} else {
+        echo 'Não foi encontrado nenhum post</p>';
+      }?>
+
+    </div>
+    <?php get_sidebar(); ?>
+  </div>
+<?php } else{ ?>
+  <?php
   if(have_posts()){
     while(have_posts()){the_post();
       get_template_part('content', get_post_format());
     ?>
 
-<?php }} else {
+  <?php }} else {
     echo 'Não foi encontrado nenhum post</p>';
-  }
+  }} ?>
 
-get_footer();
 
- ?>
+<?php get_footer(); ?>
