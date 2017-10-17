7a62928fc731469c26ba3c3ae4a95cea4c1d2ff4
Victor Piccoli <victorpicc@gmail.com>
2017-10-17 16:49:50 -0200

    email encaminhar padrao

---
+++ wp-content/themes/pergunteespecialista/inc/std_mail.php
@@ -0,0 +1,8 @@
+Prezado(a) colega,
+a pergunta abaixo foi encaminhada ao "<?php echo get_bloginfo('name'); ?>" Pensamos que você poderia colaborar elaborando uma resposta a essa questão.
+
+Caso tenha interesse e disponibilidade para a respondê-la dentro de cerca de uma semana, sua resposta poderá ser enviada em resposta a esta mensagem.
+
+Por favor, nos informe de imediato se pretende respondê-la ou não, para que, se for o caso, encaminharmos a outro colega.
+
+Desde já agradecemos
